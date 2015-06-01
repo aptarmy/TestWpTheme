c44ac791b1bca4946d0e56e2b6fee7c74b3ad30c
aptarmy <apt.enjoy@gmail.com>
2015-06-01 11:28:13 +0700

    Adopt accordion menu to main project

---
+++ js/main.js
@@ -0,0 +1,5 @@
+$(".widgetMenuSlide > ul > li").click(function(){
+	$(".widgetMenuSlide > ul ul").not($(this).find("ul")).slideUp()
+		.end().find("a").click(function(e){e.stopPropagation();});
+	$(this).find("ul").slideToggle();
+});
