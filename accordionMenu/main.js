0123950dd24925ec10905d34f01120f686ee15c0
aptarmy <apt.enjoy@gmail.com>
2015-06-01 11:40:12 +0700

    delete accordion menu demo folder

--- accordionMenu/main.js
+++
@@ -1,6 +0,0 @@
-(function($){
-	$("#accordian h3").click(function(){
-		$("#accordian h3").siblings("ul").slideUp();
-		$(this).siblings("ul").slideDown();
-	});
-})(jQuery);
--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 $(".widgetMenuSlide > ul > li").click(function(){
-	$(".widgetMenuSlide > ul ul").not($(this).find("ul")).slideUp()
-		.end().find("a").click(function(e){e.stopPropagation();});
+	$(".widgetMenuSlide > ul ul").not($(this).find("ul")).slideUp().end().find("a").click(function(e){e.stopPropagation();});
 	$(this).find("ul").slideToggle();
 });
